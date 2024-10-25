214ad144cfd90ec51661ce342694c218828b4b7d
Raquel Maia <raquelmaia860@gmail.com>
2024-10-25 11:24:45 -0300

    atualização da página detalhes para carregar imagem do db e ajuste de detalhes Médicos

--- projeto_abrigo_animal/animais/urls.py
+++ projeto_abrigo_animal/animais/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path 
 from .import views 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('',views.home,name='home'),
@@ -8,3 +11,6 @@ urlpatterns = [
     path('map/', views.map_view, name='map'),
     
 ]
+
+if settings.DEBUG:  # Isso é importante para garantir que a configuração de mídia só seja aplicada no modo de desenvolvimento
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- projeto_abrigo_animal/projeto_abrigo_animal/settings.py
+++ projeto_abrigo_animal/projeto_abrigo_animal/settings.py
@@ -16,6 +16,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR)
 STATICFILES_DIRS = [
