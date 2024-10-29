f3bb6b7774d28abac83df5c078b8f2e672b5598d
Raquel Maia <raquelmaia860@gmail.com>
2024-10-29 16:38:47 -0300

    tudo configurado home e detalhes

--- projeto_abrigo_animal/animais/views.py
+++ projeto_abrigo_animal/animais/views.py
@@ -12,25 +12,32 @@ from django.contrib.auth.decorators import login_required
 
 # ----------------------------------------------------------------------- HOME
 
-def home(request):
-    form = AnimalForm()  # Cria uma instância do formulário
-    animais = Animal.objects.all()  # Recupera todos os animais do banco de dados
+from django.shortcuts import render
+from django.views.decorators.cache import never_cache
+from .forms import AnimalForm
+from .models import Animal
 
+@never_cache  # Desativa o cache da view
+def home(request):
+    form = AnimalForm()  
+    animais = Animal.objects.all()  
+
+    # Se algum parâmetro GET é passado, aplica os filtros
     if request.method == "GET":
-        nome_pet = request.GET.get('nome_pet')
-        especie = request.GET.get('especie')  # O ID da espécie não é mais necessário
-        genero = request.GET.get('genero')
-        porte = request.GET.get('porte')
-        estado = request.GET.get('estado')
-        cidade = request.GET.get('cidade')
+        nome_pet = request.GET.get('nome_pet', '')
+        tipo = request.GET.get('tipo', '')
+        sexo = request.GET.get('sexo', '')
+        porte = request.GET.get('porte', '')
+        estado = request.GET.get('estado', '')
+        cidade = request.GET.get('cidade', '')
 
         # Filtrando os animais com base nos critérios fornecidos
         if nome_pet:
             animais = animais.filter(nome__icontains=nome_pet)
-        if especie:  # Assumindo que 'especie' é uma string correspondente a 'tipo'
-            animais = animais.filter(tipo=especie)
-        if genero:
-            animais = animais.filter(sexo=genero)
+        if tipo:
+            animais = animais.filter(tipo=tipo)
+        if sexo:
+            animais = animais.filter(sexo=sexo)
         if porte:
             animais = animais.filter(porte=porte)
         if estado:
@@ -38,7 +45,18 @@ def home(request):
         if cidade:
             animais = animais.filter(cidade=cidade)
 
-    return render(request, 'home.html', {'animais': animais, 'form': form})
+    # Renderiza a página com o formulário e a lista filtrada
+    return render(request, 'home.html', {
+        'animais': animais,
+        'form': form,
+        'nome_pet': nome_pet,
+        'tipo': tipo,
+        'sexo': sexo,
+        'porte': porte,
+        'estado': estado,
+        'cidade': cidade,
+    })
+
 
 
 
@@ -135,14 +153,14 @@ def solicitacao_adocao(request, id):
         form = AdocaoForm()
 
     return render(request, 'solicitacao_adocao.html', {'form': form, 'animal': animal})
-     
+
 
 # ----------------------------------------------------------------------- LIVIA
 
 
 def quero_ajudar(request):
     return render(request, 'quero_ajudar.html')
-     
+
 def sobre_nos(request):
     return render(request, 'sobre_nos.html')
 
